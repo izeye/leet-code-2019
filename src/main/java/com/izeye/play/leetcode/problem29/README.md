6439dfb776eecf9bba23b908f071f5ab623f57e2
Johnny Lim <izeye@naver.com>
2019-06-12 22:39:58 +0900

    Solve problem 29, "Divide Two Integers"
    See https://leetcode.com/problems/divide-two-integers/
    
    This has been cheated with the following: https://www.geeksforgeeks.org/divide-two-integers-without-using-multiplication-division-mod-operator/

--- src/main/java/com/izeye/play/leetcode/problem29/README.md
+++ src/main/java/com/izeye/play/leetcode/problem29/README.md
@@ -2,5 +2,5 @@ The final result is as follows (cheated with division):
 
 ```
 Runtime: 1 ms, faster than 100.00% of Java online submissions for Divide Two Integers.
-Memory Usage: 33.6 MB, less than 43.95% of Java online submissions for Divide Two Integers.
+Memory Usage: 33.6 MB, less than 42.73% of Java online submissions for Divide Two Integers.
 ```
--- src/main/java/com/izeye/play/leetcode/problem29/Solution.java
+++ src/main/java/com/izeye/play/leetcode/problem29/Solution.java
@@ -5,14 +5,43 @@ package com.izeye.play.leetcode.problem29;
  *
  * See https://leetcode.com/problems/divide-two-integers/
  *
+ * This has been cheated with the following: https://www.geeksforgeeks.org/divide-two-integers-without-using-multiplication-division-mod-operator/
+ *
  * @author Johnny Lim
  */
 class Solution {
 
 	public int divide(int dividend, int divisor) {
-		// FIXME: Don't cheat with division.
-		long quotient = ((long) dividend) / divisor;
-		return quotient > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) quotient;
+		long dividendAsLong = dividend;
+		long divisorAsLong = divisor;
+		boolean negativeDividend = false;
+		if (dividendAsLong < 0) {
+			dividendAsLong = -dividendAsLong;
+			negativeDividend = true;
+		}
+		boolean negativeDivisor = false;
+		if (divisorAsLong < 0) {
+			divisorAsLong = -divisorAsLong;
+			negativeDivisor = true;
+		}
+		boolean negative = negativeDividend ^ negativeDivisor;
+		long quotient = 0;
+		long temp = 0;
+		for (int i = 31; i >= 0; i--) {
+			long shifted = divisorAsLong << i;
+			if (temp + shifted <= dividendAsLong) {
+				temp += shifted;
+				quotient |= 1L << i;
+			}
+		}
+		long result = negative ? -quotient : quotient;
+		if (result > Integer.MAX_VALUE) {
+			return Integer.MAX_VALUE;
+		}
+		if (result < Integer.MIN_VALUE) {
+			return Integer.MIN_VALUE;
+		}
+		return (int) result;
 	}
 
 }
